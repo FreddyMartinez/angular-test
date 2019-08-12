2ec470685f55ea9e932860324f47c53de527fdfb
FreddyMartinez <fhmartinezq@unal.edu.co>
2019-08-11 23:33:24 -0500

    example animation is created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { slideInAnimation } from './shared/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [slideInAnimation]
 })
 export class AppComponent {
-  title = 'hiberus-test';
 }
